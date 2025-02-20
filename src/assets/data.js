a6493f1dd842c033391b21a8279feb3096754917
Adarsh c <122138848+Skythrilll@users.noreply.github.com>
2025-02-20 14:53:51 +0530

    Add files via upload

---
+++ src/assets/data.js
@@ -0,0 +1,49 @@
+export const data =[
+    {
+        
+        question: 'Which device is required for the internet connection?',
+        option1:'Modem',
+        option2:'Router',
+        option3:'Lan Cable',
+        option4:'Pen Drive',
+
+        answer: 1,
+    },
+    {
+        
+        question: 'What is the full form of RAM?',
+        option1: 'Random Access Memory', 
+        option2: 'Randomely Access Memory',
+        option3: 'Run Accept Memory',
+        option4: 'None of these',
+
+        answer: 1,
+    },
+    {
+        
+        question: 'What is the full form of CPU?',
+        option1: 'Central Program Unit',
+        option2:'Central Processing Unit',
+        option3:'Computer Personal Unit',
+        option4:'None of these',
+        answer: 2,
+    },
+    {
+        
+        question: 'What is the full form of E-mail?',
+        option1: 'Electronic Mail',
+        option2:'Electric Mail',
+        option3:'Engine Mail',
+        option4:'None of these',
+        answer: 1,
+    },
+    {
+        
+        question: 'What is the full form of HTML?',
+        option1: 'Hyper Text Markup Language',
+        option2: 'High Text Markup Language',
+        option3:  'Hyper Tabular Markup Language',
+        option4:   'None of these',
+        answer: 1,
+    }
+]
